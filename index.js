8044c19020f9ec2af4c91347620d4dc5656baad1
zhangsong <392265312@qq.com>
2021-02-18 11:53:56 +0800

    使用es module

--- index.js
+++ index.js
@@ -1,65 +1,67 @@
+import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js";
+
 new Vue({
-	el: "#app",
-	data() {
-		return {
-			todoLists: [],
-			listsView: [],
-			input: "",
-			isChecked: "all",
-			startId: 0
-		};
-	},
-	computed: {
-		isDisabled() {
-			if (this.input.length > 0 && this.isChecked != "done") {
-				return null;
-			} else {
-				return "disabled";
-			}
-		},
-		numberOfDone() {
-			return this.todoLists.filter(obj => obj.done).length;
-		}
-	},
-	watch: {
-		isChecked() {
-			this.updateView();
-		},
-		todoLists() {
-			this.listsView = this.todoLists;
-		}
-	},
-	methods: {
-		addTodoList() {
-			if (this.isDisabled == "disabled") {
-				return;
-			}
-			this.todoLists.push({
-				id: ++this.startId,
-				todo: this.input,
-				done: false
-			});
-			this.input = "";
-		},
-		deletTodoList(index) {
-			this.todoLists.splice(index, 1);
-		},
-		changeStatus(list) {
-			list.done = !list.done;
-			this.updateView();
-		},
-		updateView() {
-			switch (this.isChecked) {
-				case "all":
-					this.listsView = this.todoLists;
-					break;
-				case "done":
-					this.listsView = this.todoLists.filter(obj => obj.done);
-					break;
-				case "todo":
-					this.listsView = this.todoLists.filter(obj => !obj.done);
-					break;
-			}
-		}
-	}
+  el: "#app",
+  data() {
+    return {
+      todoLists: [],
+      listsView: [],
+      input: "",
+      isChecked: "all",
+      startId: 0,
+    };
+  },
+  computed: {
+    isDisabled() {
+      if (this.input.length > 0 && this.isChecked != "done") {
+        return null;
+      } else {
+        return "disabled";
+      }
+    },
+    numberOfDone() {
+      return this.todoLists.filter((obj) => obj.done).length;
+    },
+  },
+  watch: {
+    isChecked() {
+      this.updateView();
+    },
+    todoLists() {
+      this.listsView = this.todoLists;
+    },
+  },
+  methods: {
+    addTodoList() {
+      if (this.isDisabled == "disabled") {
+        return;
+      }
+      this.todoLists.push({
+        id: ++this.startId,
+        todo: this.input,
+        done: false,
+      });
+      this.input = "";
+    },
+    deletTodoList(index) {
+      this.todoLists.splice(index, 1);
+    },
+    changeStatus(list) {
+      list.done = !list.done;
+      this.updateView();
+    },
+    updateView() {
+      switch (this.isChecked) {
+        case "all":
+          this.listsView = this.todoLists;
+          break;
+        case "done":
+          this.listsView = this.todoLists.filter((obj) => obj.done);
+          break;
+        case "todo":
+          this.listsView = this.todoLists.filter((obj) => !obj.done);
+          break;
+      }
+    },
+  },
 });
