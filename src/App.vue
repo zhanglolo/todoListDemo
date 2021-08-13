b880fe8597cef9858ade08b4ed8e7c08c3c4cf69
zhanglolo from mac <392265312@qq.com>
2021-08-13 22:08:00 +0800

    调整css

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-blue-300 h-full flex flex-col space-y-8">
+  <div class="bg-blue-300 h-full flex flex-col space-y-8 text-center">
     <h1 class="text-6xl text-warm-gray-100">我的待办清单V2</h1>
     <h3 class="text-4xl text-warm-gray-50">
       当前共有{{ todoLists.length }}个代办事项， 其中{{
@@ -29,14 +29,24 @@
     <div class="flex justify-center space-x-3">
       <input
         type="text"
-        class="w-100 h-10 rounded-1xl text-left text-xl px-4"
+        class="w-100 h-10 rounded-1xl text-left text-xl px-4 shadow-md"
         placeholder="请输入待办事项"
         @keyup.enter="addTodoList"
         v-model.trim="input"
       />
       <input
         type="button"
-        class="w-20 h-10 rounded-1xl bg-red-400 text-light-200"
+        class="
+          w-20
+          h-10
+          rounded-1xl
+          bg-red-500
+          text-warm-gray-100
+          hover:bg-red-600
+          shadow-md
+          disabled:(bg-red-200
+          text-warm-gray-400)
+        "
         value="添加"
         @click="addTodoList"
         :disabled="isDisabled"
@@ -124,7 +134,6 @@ watchEffect(() => {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
-  text-align: center;
 }
 html {
   height: 100vh;
--- src/components/todoListItem.vue
+++ src/components/todoListItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <li class="flex justify-between items-baseline text-2xl text-white">
+  <li class="flex justify-between items-baseline text-2xl text-white space-y-4">
     <span class="inline-block mr-6">{{ index + 1 }}</span>
     <span class="inline-block w-100 text-left">{{ list.todo }}</span>
     <span class="inline-block cursor-pointer" @click="$emit('change-status')">{{
